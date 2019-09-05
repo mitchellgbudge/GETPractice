f7546eae784879eada03a0002abab4735137e453
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-04 20:54:31 -0600

    Modify networking methods

--- AmiiboInfo/AmiiboInfo/AmiiboController.swift
+++ AmiiboInfo/AmiiboInfo/AmiiboController.swift
@@ -8,3 +8,37 @@
 
 import Foundation
 
+var amiibos: [AmiiboResult] = []
+
+func fetchAmiibos(completion: @escaping (Result<AmiiboResult, Error>) -> Void) {
+    
+    let baseURL = URL(string: "https://www.amiiboapi.com/api/amiibo/")!
+    
+    URLSession.shared.dataTask(with: baseURL) { (data, response, error) in
+        
+        if let error = error {
+            print("Error fetching amiibos \(error)")
+            completion(.failure(error))
+            return
+        }
+        
+        guard let data = data else {
+            completion(.failure(NSError()))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            let amiibo = try decoder.decode(AmiiboResult.self, from: data)
+            amiibos.append(amiibo)
+        } catch {
+            NSLog("Error decoding data to type Pokemon: \(error)")
+            completion(.failure(error))
+        }
+        
+        }.resume()
+}
+
+
